820a92d20606f5e790f045ab496c2cd7627b5a25
Matt March <matt.marchcoding@gmail.com>
2023-10-03 16:19:48 -0400

    updating with render info

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ displayFaceBox = (box) =>{
       .then(() => {
         console.log('hi', res)
         if (res.status === 200) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://mynodeserver1.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
